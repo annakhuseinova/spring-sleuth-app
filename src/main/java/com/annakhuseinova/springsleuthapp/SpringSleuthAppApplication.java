6b350b8b9f64d03b79468c399a09754e81479420
annakhuseinova <annakhuseinova@yandex.ru>
2021-04-10 21:14:03 +0300

    Initial

---
+++ src/main/java/com/annakhuseinova/springsleuthapp/SpringSleuthAppApplication.java
@@ -0,0 +1,13 @@
+package com.annakhuseinova.springsleuthapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSleuthAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSleuthAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/annakhuseinova/springsleuthapp/SpringSleuthAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.annakhuseinova.springsleuthapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSleuthAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
