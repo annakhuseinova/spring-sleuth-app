56cf0a9749c9f0f771eacdc0373b3104403b9383
annakhuseinova <annakhuseinova@yandex.ru>
2021-04-11 01:28:48 +0300

    Added prometheus config

--- src/main/java/com/annakhuseinova/springsleuthapp/service/EsbImitationServiceImpl.java
+++ src/main/java/com/annakhuseinova/springsleuthapp/service/EsbImitationServiceImpl.java
@@ -3,8 +3,10 @@ package com.annakhuseinova.springsleuthapp.service;
 import com.annakhuseinova.springsleuthapp.dto.ActivationDto;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.micrometer.core.annotation.Timed;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.sleuth.annotation.NewSpan;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -14,6 +16,8 @@ public class EsbImitationServiceImpl implements EsbImitationService {
 
     private final ObjectMapper objectMapper;
 
+    @Timed(value = "activation-function-metric")
+    @NewSpan(name = "activation-function")
     @Override
     public String activate(ActivationDto activationDto) throws InterruptedException {
         String json = null;
